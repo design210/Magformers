8bf02c511d46efa4c026b3a15a096e69d54b6203
design210 <design210@nate.com>
2018-05-11 13:54:02 +0900

    언어 셀렉트 삭제 처리

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,7 @@
         <div v-for="post in posts" :key="post.id">
         <div class="warning_tt">
             <h2>{{ post.txt25 }}</h2>								
-        </div>						
-         <select v-model="selected" id="select">
-            <option v-for="option in options" v-bind:value="option.value" :key="option.value">
-                {{ option.text }}
-            </option>
-        </select>
+        </div>        
         <div class="clear"></div>        
             <div class="wan_title">
                 <span>{{ post.title }}</span>
@@ -152,41 +147,7 @@ export default {
     return {
         posts: [],
         errors: [],
-        selected: 'English',
-        options: [
-            { text: 'English', value: 'English' },
-            { text: 'Afrikaans', value: 'Afrikaans' },
-            { text: 'Bahasa Indonesia', value: 'Indonesia' },
-            { text: 'Català', value: 'Catal' },
-            { text: 'Čeština', value: 'cestina' },
-            { text: 'Dansk', value: 'Dansk' },
-            { text: 'Deutsch', value: 'Deutsch' },
-            { text: 'Eesti', value: 'Eesti' },
-            { text: 'Español', value: 'Espanol' },
-            { text: 'Íslenska', value: 'islenska' },
-            { text: 'Polski', value: 'Polski' },
-            { text: 'Latviešu', value: 'Latviesu' },
-            { text: 'Français', value: 'Francais' },
-            { text: 'Lietuvių kalba', value: 'Lietuviu' },
-            { text: 'Italiano', value: 'Italiano' },
-            { text: 'Magyar', value: 'Magyar' },
-            { text: 'Nederlands', value: 'Nederlands' },
-            { text: 'Norsk', value: 'Norsk' },
-            { text: 'Português', value: 'Portugues' },
-            { text: 'Slovenčina', value: 'Slovencina' },
-            { text: 'Suomi', value: 'Suomi' },
-            { text: 'Svenska', value: 'Svenska' },
-            { text: 'iếng Việt', value: 'Viet' },
-            { text: 'Монгол', value: 'Mongo' },
-            { text: 'Русский', value: 'Russia' },
-            { text: 'Українська мова', value: 'Ukraina' },
-            { text: 'עברית', value: 'Hibri' },
-            { text: 'ภาษาไทย', value: 'Tai' },
-            { text: '日本語', value: 'Japanese' },
-            { text: '中文(繁體)', value: 'Chaina1' },
-            { text: '中文(简体)', value: 'Chaina2' },
-            { text: '한국어', value: 'Korean' }
-        ]
+        selected: 'English'
     }
   },
   created() {
