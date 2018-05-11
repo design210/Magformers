9935f7eec0ad8d5a05401c2ce25faa154ca6d805
design210 <design210@nate.com>
2018-05-11 13:57:18 +0900

    다국어 처리

--- src/App.vue
+++ src/App.vue
@@ -152,7 +152,41 @@ export default {
     return {
         posts: [],
         errors: [],
-        selected: 'English'
+        selected: 'English',
+        options: [
+            { text: 'English', value: 'English' },
+            { text: 'Afrikaans', value: 'Afrikaans' },
+            { text: 'Bahasa Indonesia', value: 'Indonesia' },
+            { text: 'Català', value: 'Catal' },
+            { text: 'Čeština', value: 'cestina' },
+            { text: 'Dansk', value: 'Dansk' },
+            { text: 'Deutsch', value: 'Deutsch' },
+            { text: 'Eesti', value: 'Eesti' },
+            { text: 'Español', value: 'Espanol' },
+            { text: 'Íslenska', value: 'islenska' },
+            { text: 'Polski', value: 'Polski' },
+            { text: 'Latviešu', value: 'Latviesu' },
+            { text: 'Français', value: 'Francais' },
+            { text: 'Lietuvių kalba', value: 'Lietuviu' },
+            { text: 'Italiano', value: 'Italiano' },
+            { text: 'Magyar', value: 'Magyar' },
+            { text: 'Nederlands', value: 'Nederlands' },
+            { text: 'Norsk', value: 'Norsk' },
+            { text: 'Português', value: 'Portugues' },
+            { text: 'Slovenčina', value: 'Slovencina' },
+            { text: 'Suomi', value: 'Suomi' },
+            { text: 'Svenska', value: 'Svenska' },
+            { text: 'iếng Việt', value: 'Viet' },
+            { text: 'Монгол', value: 'Mongo' },
+            { text: 'Русский', value: 'Russia' },
+            { text: 'Українська мова', value: 'Ukraina' },
+            { text: 'עברית', value: 'Hibri' },
+            { text: 'ภาษาไทย', value: 'Tai' },
+            { text: '日本語', value: 'Japanese' },
+            { text: '中文(繁體)', value: 'Chaina1' },
+            { text: '中文(简体)', value: 'Chaina2' },
+            { text: '한국어', value: 'Korean' }
+        ]
     }
   },
   created() {
